cb7f2e5e0bffd3cf06b984f507244b67283a64b3
Siferga <sifergadeveloper@gmail.com>
2023-10-12 12:38:29 +0200

    adding testimonial.js

---
+++ src/components/Testimonial.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Testimonial() {
+  return (
+    <div className='container-testimonial'>
+      <img 
+      className='image-testimonial'
+      src= {require('../images/testimonial-emma.pgn')}
+      alt='Foto 1'
+      />
+    </div>
+  );
+
+}
