1512f13d3a5daf39a5491edb3d30b3c5f96f4eac
Siferga <sifergadeveloper@gmail.com>
2023-10-17 10:47:01 +0200

    Component Testimonial

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -5,10 +5,16 @@ function Testimonial() {
     <div className='container-testimonial'>
       <img 
       className='image-testimonial'
-      src= {require('../images/testimonial-emma}.pgn')}
-      alt='Foto emma'
-      />
+      src= {require('../images/testimonial-emma.png')}
+      alt='Fotoemma'/>
+      <div className='container-text-testimonial'>
+        <p className='name-testimonial'>Emma Bostiar</p>
+        <p className='profession-testimonial'>Ingeniera en Spotify</p>
+        <p className='texto-testimonial'>"Siempre he tenido problemas para aprondre JavaScript. El realizar un curso de freeCodeCamp me ayudo a comprendrer et adquirir los conocimientos necesarios para mi trabajo como ingeniero de software en Spotify." </p>
+      </div>    
     </div>
+  
   );
 
 }
+export default Testimonial;
